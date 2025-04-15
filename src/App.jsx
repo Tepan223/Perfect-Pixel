860ff451e3724dc18c87866d3b05d942c7a3fa89
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-04-15 13:04:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="blank">
+      <div className="container">
+        <div className='text'>
+          <p className='title'>Kecerdasan Visual</p>
+          <p className='subtitle'>Lihat dunia dari <br/> lensa yang baru.</p>
+          <p className='description'>
+            Gunakan <span>kecerdasan visual </span><br/>
+            melalui Kontrol Kamera untuk<br/>
+            langsung mempelajari tentang<br/>
+            semua hal yang Anda liat.<br/>
+            Cukup <span>arahkan iPhone 16 Pro<br/>
+            Anda untuk menemukan lebih </span><br/>
+            atau berinteraksi dengan<br/>
+            sesuatu di depan Anda. Cari<br/>
+            tahu di mana tempat membeli<br/>
+            barang baru yang Anda lihat,<br/>
+            identifikasikan tanaman dan hewan, dan<br/>
+             banyak lagi.
+           </p>
+        </div>
+        <div className='image'>
+          <img src="Phone.png" alt="" />
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
